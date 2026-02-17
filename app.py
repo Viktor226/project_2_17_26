f15bbf08687450d30e9c38c6fdd44c5cd558d623
Viktor226 <vanistratenko.a.5@gmail.com>
2026-02-17 14:09:57 +0200

    Implement Book Checker App with Streamlit
    Added a Streamlit app to check and add books to a database.

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-
+import streamlit as st
+books = [
+  "The Hobbit"
+  "1984"
+  "Harry Potter"
+  "Sherlok Holms"
+  "Top gear"
+  
+]
+st.title("Book Checker App")
+st.write("Enter a book title to check if it exist in the database.")
+user_input =st.text_input("Book Title")
+if st.button("Check Book")
+  if user_input.strip() == "":
+    st.warning("Please enter a book title")
+  elif user_input in books:
+    st.success("The book exist in database!")
+  else :
+    st.error("The book is NOT in the database")
+    st.write("you can add this book in database")
+    if st.button("add book"):
+      books.append(user_input)
+      st.write("book" user input,"has been added to database") 
+      
+    
